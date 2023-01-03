9242d1f10bc1ddaace1e52579604ae0d4561cf3f
Vladislav Aripov <v.aripov@hightech.group>
2023-01-03 14:22:49 +0300

    Нарисовали лендинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.svg';
 
-function App() {
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>
+        <div className='logo'>
+          <img className='logo-img' src={logo} alt='' />
+        </div>
+        <div className='nav-bar'>
+          <div className='nav'>Мониторинг</div>
+          <div className='nav'>О нас</div>
+          <div className='nav'>Контакты</div>
+        </div>
+      </div>
+      <div className='body'>
+        <div className='about'>
+          <div className="title">Lorem ipsum</div>
+          <div className="content">
+            {loremIpsum}
+          </div>
+        </div>
+      </div>
+      <div className='footer'>
+        <div className='contacts'>
+          Контакты:
+          <div className='contact'>+7 939.. - Владислав</div>
+          <div className='contact'>+7 939.. - Владислав</div>
+          <div className='contact'>+7 939.. - Владислав</div>
+          <div className='contact'>+7 939.. - Владислав</div>
+        </div>
+        <div className='rights'>
+          © Все права защищены. ООО "Фрукты жи есть".
+        </div>
+      </div>
+    </>
   );
 }
 

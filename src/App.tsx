1c76ecb9f13779274af27d822942ae92cb69742e
Vladislav Aripov <v.aripov@hightech.group>
2023-01-04 17:58:15 +0300

    добавили товары, добавили функциональные компоненты и переходы по секциям

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import NavBar from './components/NavBar';
+import Products from './components/Products';
 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
@@ -11,22 +13,37 @@ const App: React.FC = () => {
         <div className='logo'>
           <img className='logo-img' src={logo} alt='' />
         </div>
-        <div className='nav-bar'>
-          <div className='nav'>Мониторинг</div>
-          <div className='nav'>О нас</div>
-          <div className='nav'>Контакты</div>
-        </div>
+        <NavBar
+          items={[
+            {
+              title: 'Мониторинг',
+              link: '/#products-area',
+            },
+            {
+              title: 'О нас',
+              link: '/#about-area',
+            },
+            {
+              title: 'Контакты',
+              link: '/#contacts',
+            },
+          ]}
+        />
       </div>
       <div className='body'>
-        <div className='about'>
+        <div className='about-area' id='about-area'>
           <div className="title">Lorem ipsum</div>
           <div className="content">
             {loremIpsum}
           </div>
         </div>
+        <div className="products-area" id='products-area'>
+          <div className="title">Наши услуги</div>
+          <Products />
+        </div>
       </div>
       <div className='footer'>
-        <div className='contacts'>
+        <div className='contacts' id='contacts'>
           Контакты:
           <div className='contact'>+7 939.. - Владислав</div>
           <div className='contact'>+7 939.. - Владислав</div>
---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,52 @@
+import Product, { IProduct } from './components/Product';
+import './Products.css';
+import analys from '../../assets/analys.png';
+import calculateZP from '../../assets/calculateZP.jpg';
+import calculateNalogVichet from '../../assets/calculateNalogVichet.jpeg';
+import calculateNalogBase from '../../assets/calculateNalogBase.jpg';
+import timeTracking from '../../assets/timeTracking.jpg';
+
+const Products: React.FC = () => {
+    const products: IProduct[] = [
+        {
+            image: timeTracking,
+            title: 'Учет рабочего времени',
+            price: 0,
+            description: 'Описание',
+        },
+        {
+            image: calculateZP,
+            title: 'Расчет заработной платы',
+            price: 0,
+            description: 'Описание',
+        },
+        {
+            image: calculateNalogVichet,
+            title: 'Расчет налогового вычета',
+            price: 0,
+            description: 'Описание',
+        },
+        {
+            image: analys,
+            title: 'Анализ налоговой базы',
+            price: 0,
+            description: 'Описание',
+        },
+        {
+            image: calculateNalogBase,
+            title: 'Расчет налоговой базы',
+            price: 0,
+            description: 'Описание',
+        },
+    ];
+
+    return (
+        <div className="products">
+            {
+                products.map((item) => <Product item={item} />)
+            }
+        </div>
+    );
+};
+
+export default Products;
